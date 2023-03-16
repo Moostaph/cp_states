9b4cf062763843910efb3e65351c206fc9224078
Moostaph <mbayemouha89@gmail.com>
2023-03-16 13:31:33 +0000

    Le premier commit les etats react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        fullName: 'Mouhamadou Moustapha MBAYE',
+        bio: 'Administrateur reseaux et Systèmes voulant se reconvertir en Dev.',
+        imgSrc: 'https://nappy.co/photo/2383',
+        profession: 'Developer'
+      },
+      show: false
+    };
+  }
+  handleClick = () => {
+    this.setState({
+      show: !this.state.show
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.handleClick}>Mon Profile</button>
+        {this.state.show && (
+          <div>
+            <h2>{this.state.person.fullName}</h2>
+            <img src={this.state.person.imgSrc} alt="profile" />
+            <p>{this.state.person.bio}</p>
+            <p>{this.state.person.profession}</p>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
+
